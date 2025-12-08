dcbe99fd944c9fe01c0ed52d6c9a39f210f18955
Flurin Sturzenegger <st.flurin@gmail.com>
2025-12-08 11:23:45 +0100

    create task_list; update manifest

--- src/maps_omezarr_converter/dev/task_list.py
+++ src/maps_omezarr_converter/dev/task_list.py
@@ -1,20 +1,28 @@
 """Contains the list of tasks available to fractal."""
 
 from fractal_task_tools.task_models import (
-    ParallelTask,
+    ConverterCompoundTask,
 )
 
 AUTHORS = "Flurin Sturzenegger"
 DOCS_LINK = None
 INPUT_MODELS = []
 
-# TASK_LIST = [
-#     ParallelTask(
-#         name="Thresholding Label Task",
-#         executable="thresholding_label_task.py",
-#         meta={"cpus_per_task": 1, "mem": 4000},
-#         category="Segmentation",
-#         tags=["Instance Segmentation", "Classical segmentation"],
-#         docs_info="file:docs_info/thresholding_task.md",
-#     ),
-# ]
+
+TASK_LIST = [
+    ConverterCompoundTask(
+        name="Convert MAPS to OME-Zarr",
+        executable_init="convert_maps_init_task.py",
+        executable="convert_maps_compute_task.py",
+        meta_init={"cpus_per_task": 1, "mem": 4000},
+        meta={"cpus_per_task": 1, "mem": 12000},
+        category="Conversion",
+        tags=[
+            "MAPS",
+            "EM",
+            "ThermoFisher",
+            "Converter",
+        ],
+        #docs_info="file:docs_info/convert_nd2_task.md",
+    ),
+]
